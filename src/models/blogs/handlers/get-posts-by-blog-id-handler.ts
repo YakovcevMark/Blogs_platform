29347a4a329ae3193c1f12cc31300412d6bb80ef
Mark <yakovcevmark.dev@gmail.com>
2025-11-19 18:27:31 +0300

    small fix v3

--- src/models/blogs/handlers/get-posts-by-blog-id-handler.ts
+++ src/models/blogs/handlers/get-posts-by-blog-id-handler.ts
@@ -2,9 +2,18 @@ import {Request, Response} from 'express'
 import {matchedData} from "express-validator";
 import {postsService} from "../../posts/application/posts.service";
 import {PostsQueryList} from "../../posts/types/posts.query.list";
+import {blogsService} from "../application/blogs.service";
+import {HTTP_STATUS_CODES} from "../../../core/enums/http-status-codes";
 
 export const getPostsByBlogIdHandler = async (req: Request<{ blogId: string }>, res: Response) => {
     const queryParamsFromValidator = matchedData(req, {locations: ['query']}) as Omit<PostsQueryList, 'blogId'>;
+
+    const isBlogPersistInDb = await blogsService.isPersistInDb(req.params.blogId)
+
+    if (!isBlogPersistInDb) {
+        res.sendStatus(HTTP_STATUS_CODES.NOT_FOUND_404);
+    }
+
     const blogs = await postsService.getAll({...queryParamsFromValidator, blogId: req.params.blogId});
     res.send(blogs)
 }
