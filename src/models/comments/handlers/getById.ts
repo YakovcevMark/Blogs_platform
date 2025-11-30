fd7f9d50be198f4a8dc509984a192706103d1fba
Mark <yakovcevmark.dev@gmail.com>
2025-11-30 17:44:57 +0300

    small fix

--- src/models/comments/handlers/getById.ts
+++ src/models/comments/handlers/getById.ts
@@ -1,8 +1,13 @@
 import {Request, Response} from 'express'
 import {commentsQueryRepository} from "../repositories/query-repo";
 import {RequestEntityId} from "../../../core/types";
+import {HTTP_STATUS_CODES} from "../../../core/enums/http-status-codes";
 
 export const getCommentByIdHandler = async (req: Request<RequestEntityId>, res: Response) => {
     const comment = await commentsQueryRepository.getById({id:req.params.id});
+    if(!comment) {
+        res.sendStatus(HTTP_STATUS_CODES.NOT_FOUND_404)
+        return;
+    }
     res.send(comment)
 }
